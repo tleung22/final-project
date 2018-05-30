73a59a3ca1a3d77da67f8246c844ff94ad5b0f66
Jose <Jose@D-10-18-233-253.dhcp4.washington.edu>
2018-05-29 20:19:12 -0700

    updated files

--- server.R
+++ server.R
@@ -1,37 +1,62 @@
+
 library(shiny)
-library(shinythemes)
 library(dplyr)
 library(plotly)
 
 mass_shootings <-
-  read.csv("Data/Mother Jones - Mass Shootings Database, 1982 - 2018 - Sheet1.csv", stringsAsFactors = FALSE)
-happiness <- read.csv("Data/state_happiness.csv", stringsAsFactors = FALSE)
-gun_industry <- read.csv("Data/gun_industry.csv", stringsAsFactors = FALSE)
-legislation <- read.csv("Data/legislation.csv", stringsAsFactors = FALSE)
-gun_crime <- read.csv("Data/US gun crime - SUMMARY 2011.csv", stringsAsFactors = FALSE)
-gun_ownership <- read.csv("Data/gun_ownership.csv", stringsAsFactors = FALSE)
+  read.csv("Data/Mother Jones - Mass Shootings Database, 1982 - 2018 - Sheet1.csv",
+           stringsAsFactors = FALSE
+  )
+happiness <- read.csv("Data/state_happiness.csv",
+                      stringsAsFactors = FALSE
+)
+gun_industry <- read.csv("Data/gun_industry.csv",
+                         stringsAsFactors = FALSE
+)
+legislation <- read.csv("Data/legislation.csv",
+                        stringsAsFactors = FALSE
+)
+gun_crime <- read.csv("Data/US gun crime - SUMMARY 2011.csv",
+                      stringsAsFactors = FALSE
+)
+gun_ownership <- read.csv("Data/gun_ownership.csv",
+                          stringsAsFactors = FALSE
+)
 gun_ownership$percown <- as.numeric(gsub("%", "", gun_ownership$percown))
-background_checks <- read.csv("Data/nics-firearm-background-checks.csv", stringsAsFactors = FALSE)
-state_deaths <- read.csv("Data/firearm2016.csv", stringsAsFactors = FALSE)
+background_checks <- read.csv("Data/nics-firearm-background-checks.csv",
+                              stringsAsFactors = FALSE
+)
+state_deaths <- read.csv("Data/firearm2016.csv",
+                         stringsAsFactors = FALSE
+)
 
-shootings <- mass_shootings %>% 
-  group_by(state) %>% 
-  summarise(fatalities = sum(fatalities), injured = sum(injured),
-            total_victims = sum(total_victims))
+shootings <- mass_shootings %>%
+  group_by(state) %>%
+  summarise(
+    fatalities = sum(fatalities), injured = sum(injured),
+    total_victims = sum(total_victims)
+  )
 joined_data <- merge(happiness, gun_industry, by = "state")
 joined_data <- full_join(joined_data, shootings, by = "state")
 joined_data <- full_join(joined_data, state_deaths, by = "state")
 joined_data <- full_join(joined_data, gun_ownership, by = "state")
 
 legislation$state <- joined_data$state
-crime <- select(joined_data, Violent.Crime...per.100.000..2013, state, Murder.and.nonnegligent.manslaughter..per.100K..2013, percown, industry_score)
+crime <- select(
+  joined_data, Violent.Crime...per.100.000..2013, state,
+  Murder.and.nonnegligent.manslaughter..per.100K..2013,
+  percown, industry_score
+)
 legislation <- merge(legislation, crime, by = "state")
 
 my_server <- function(input, output) {
   output$interactive_map <- renderPlotly({
-    joined_data$hover <- with(joined_data, paste(state, "<br>", "Happiness Rank:", happiness_rank,
-                              "<br>", "Gun Industry Rank:", industry_rank,
-                              "<br>", "Total Victims:", total_victims))
+    joined_data$hover <- with(joined_data, paste(
+      state, "<br>",
+      "Happiness Rank:", happiness_rank,
+      "<br>", "Gun Industry Rank:", industry_rank,
+      "<br>", "Total Victims:", total_victims
+    ))
     
     l <- list(color = toRGB("white"), width = 2)
     # specify some map projection/options
@@ -46,14 +71,12 @@ my_server <- function(input, output) {
       add_trace(
         z = joined_data$happiness_score, text = joined_data$hover,
         locations = joined_data$state,
-        color = joined_data$happiness_score, colors = "Reds",
-        reversescale = TRUE
+        color = joined_data$happiness_score, colors = "Reds"
       ) %>%
       colorbar(title = "Happiness Score") %>%
       layout(
         title = "Happiness and Gun Related Data for Each State",
-        geo = g,
-        margin = list(l = 50, r = 50, b = 50, t = 50)
+        geo = g
       )
     return(map)
   })
@@ -74,20 +97,29 @@ my_server <- function(input, output) {
       yaxis <- "Count of Gun Control Laws"
     }
     
-    p <- plot_ly(x = joined_data$happiness_score,
-                 color = I("black")
-                ) %>%
-      add_markers(y = y,
-                  text = paste(joined_data$state, "<br>", "Happiness Rank:",
-                               joined_data$happiness_rank),
-                  showlegend = FALSE) %>% 
-      add_lines(x = joined_data$happiness_score, y = fitted(lm(y ~ joined_data$happiness_score)),
-                line = list(color = "rgba(220, 0, 0, 0.62)"),
-                showlegend = FALSE) %>%
-      layout(title = title,
-             yaxis = list(title = yaxis, zeroline = FALSE),
-             xaxis = list(title = "Happiness Score", zeroline = FALSE),
-             margin = list(l = 50, r = 50, b = 50, t = 50))
+    p <- plot_ly(
+      x = joined_data$happiness_score,
+      color = I("black")
+    ) %>%
+      add_markers(
+        y = y,
+        text = paste(
+          joined_data$state, "<br>", "Happiness Rank:",
+          joined_data$happiness_rank
+        ),
+        showlegend = FALSE
+      ) %>%
+      add_lines(
+        x = joined_data$happiness_score,
+        y = fitted(lm(y ~ joined_data$happiness_score)),
+        line = list(color = "rgba(220, 0, 0, 0.62)"),
+        showlegend = FALSE
+      ) %>%
+      layout(
+        title = title,
+        yaxis = list(title = yaxis, zeroline = FALSE),
+        xaxis = list(title = "Happiness Score", zeroline = FALSE)
+      )
   })
   
   output$cor <- renderText({
@@ -107,15 +139,18 @@ my_server <- function(input, output) {
   
   output$cor_message <- renderText({
     if (input$yaxis == "percown") {
-      message <- "This negative correlation shows that the higher the happiness score, the lower the percentage of gun ownership."
+      message <- "This negative correlation shows that the higher the happiness
+      score, the lower the percentage of gun ownership."
     } else if (input$yaxis == "rate") {
-      message <- "This negative correlation shows that the higher the happiness score, the lower the rate of deaths per 100k people."
+      message <- "This negative correlation shows that the higher the happiness
+      score, the lower the rate of deaths per 100k people."
     } else if (input$yaxis == "lawtotal") {
-      message <- "This positive correlation shows that there is an association between higher happiness score and a higher count of gun control laws."
+      message <- "This positive correlation shows that there is an association
+      between higher happiness score and a higher count of gun control laws."
     }
     return(message)
-  })
-
+    })
+  
   output$legis_scatter <- renderPlotly({
     if (input$legislation == 1) {
       y <- legislation$industry_score
@@ -125,7 +160,7 @@ my_server <- function(input, output) {
       y <- legislation$percown
       title <- "State Gun Legislation vs State Gun Ownership Percentage"
       yaxis <- "State Percent Gun Ownership"
-    } else if (input$legislation == 3){
+    } else if (input$legislation == 3) {
       y <- legislation$Violent.Crime...per.100.000..2013
       title <- "State Gun Legislation vs State Violent Crime per 100K"
       yaxis <- "State Violent Crime per 100,000"
@@ -134,15 +169,16 @@ my_server <- function(input, output) {
       title <- "State Gun Legislation vs State Murder & Manslaughter per 100K"
       yaxis <- "State Murder & Manslaughter per 100,000"
     }
-    p <- plot_ly(legislation, x = ~lawtotal, color = I("black")) %>%
+    p <- plot_ly(legislation, x = ~ lawtotal) %>%
       add_markers(y = y, text = legislation$state, showlegend = FALSE) %>%
-      add_lines(x = ~lawtotal, y = ~fitted(loess(y ~ lawtotal)),
-                line = list(color = "rgba(220, 0, 0, 0.62)")) %>%
-      layout(yaxis = list(title = yaxis, zeroline = FALSE), xaxis = list(title = "Numer of State Gun Control Laws"),
-             title = title, margin = list(l = 50, r = 50, b = 50, t = 50))
+      add_lines(x = ~ lawtotal, y = ~ fitted(loess(y ~ lawtotal))) %>%
+      layout(
+        yaxis = list(title = yaxis, zeroline = FALSE),
+        xaxis = list(title = "Numer of State Gun Control Laws"),
+        title = title
+      )
     
     return(p)
-    
   })
   
   output$legis_cor <- renderText({
@@ -152,7 +188,7 @@ my_server <- function(input, output) {
     } else if (input$legislation == 2) {
       y <- legislation$percown
       cor <- cor(legislation$lawtotal, y)
-    } else if (input$legislation == 3){
+    } else if (input$legislation == 3) {
       y <- legislation$Violent.Crime...per.100.000..2013
       cor <- cor(legislation$lawtotal, y)
     } else {
@@ -165,16 +201,20 @@ my_server <- function(input, output) {
   
   output$legis_cor_message <- renderText({
     if (input$legislation == 1) {
-      message <- "This moderate negative correlation shows that state gun control negatively impacts the state's gun industry."
+      message <- "This moderate negative correlation shows that state
+      gun control negatively impacts the state's gun industry."
     } else if (input$legislation == 2) {
-      message <- "This moderate negative correlation shows that state gun control somewhat reduces state gun ownership."
+      message <- "This moderate negative correlation shows that state
+      gun control somewhat reduces state gun ownership."
     } else if (input$legislation == 3) {
-      message <- "This slight negative correlation shows that state gun control has a minimal effect on reducing the rate of violent crime"
+      message <- "This slight negative correlation shows that state
+      gun control has a minimal effect on reducing the rate of violent crime"
     } else {
-      message <- "This slight negative correlation shows that state gun control has a small impact on reducing the rate of murder and manslaughter"
+      message <- "This slight negative correlation shows that state gun control
+      has a small impact on reducing the rate of murder and manslaughter"
     }
     return(message)
-  })
+    })
   
   output$legislation_bar <- renderPlotly({
     shootings <- group_by(mass_shootings, state) %>%
@@ -182,14 +222,23 @@ my_server <- function(input, output) {
     legislation <- merge(legislation, shootings, all = TRUE, by = "state")
     legislation[is.na(legislation)] <- 0
     legislation <- select(legislation, total_shootings, lawtotal, state)
-    legislation$state <- factor(legislation$state, levels = unique(legislation$state)[order(legislation$total_shootings, decreasing = TRUE)])
-    p <- plot_ly(legislation, x = ~state, y = ~lawtotal, type = "bar", name = "legislation", color = I("black")) %>%
-      add_trace(y = ~total_shootings, name = "shootings", color = I("red")) %>%
-      layout(yaxis = list(title = "Count"), xaxis = list(title = "State"), title = "State Gun Control VS Mass Shootings", barmode = "group",
-             margin = list(l = 50, r = 50, b = 50, t = 50))
+    legislation$state <- factor(legislation$state,
+                                levels = unique(legislation$state)
+                                [order(legislation$total_shootings, decreasing = TRUE)]
+    )
+    p <- plot_ly(legislation,
+                 x = ~ state, y = ~ lawtotal,
+                 type = "bar", name = "legislation"
+    ) %>%
+      add_trace(y = ~ total_shootings, name = "shootings") %>%
+      layout(
+        yaxis = list(title = "Count"), xaxis = list(title = "State"),
+        title = "State Gun Control VS Mass Shootings", barmode = "group"
+      )
     
     return(p)
   })
 }
 
 shinyServer(my_server)
+
--- ui.R
+++ ui.R
@@ -5,161 +5,252 @@ library(shinythemes)
 
 ui <- navbarPage(
   # returns a theme
-  theme=shinytheme("slate"),
-  
-  "Application Title",
-  
-  
+  theme = shinytheme("slate"),
+  "Law, Happiness, & Gun Violence",
   
   # Introduction tab
   tabPanel(
     "Introduction",
     includeCSS("www/style.css"),
-    div(class = "center",
+    div(
+      class = "center",
       h1("Why It Matters"),
-      em("The United States has been vicitim to 101 mass shootings this calendar,"),
+      em("The United States has been vicitim to 101 mass
+         shootings this calendar,"),
       em("accroding to the following article by"),
       a("Business Insider",
-        href = "http://www.businessinsider.com/how-many-mass-shootings-in-america-this-year-2018-2","."),
-      em("More worrisome however, is the continious lack of action from our elected officials in Washington D.C."),
-      em("to get anything done in order to address this ongoing issue that has claimed so many innoncent lives."),
+        href =
+          "http://www.businessinsider.com/how-many
+        -mass-shootings-in-america-this-year-2018-2", "."
+      ),
+      em("More worrisome however, is the continious lack of
+         action from our elected officials in Washington D.C."),
+      em("to get anything done in order to address this ongoing
+         issue that has claimed so many innoncent lives."),
       h2("Our Purpose"),
-      em("Our mission behind this project is not take sides or shift the blame from one side to another!"),
-      em("Instead, our goal is to simply prompt awareness to an issue that has almost become like something of a"),
-      em("normality nowadays, unfortunately. Furthermore, we want to make it very clear that this no way, shape, or form"),
+      em("Our mission behind this project is not take sides
+         or shift the blame from one side to another!"),
+      em("Instead, our goal is to simply prompt awareness to an
+         issue that has almost become like something of a"),
+      em("normality nowadays, unfortunately. Furthermore,
+         we want to make it very clear that this no way, shape, or form"),
       em(" mean't to come off as a"), strong("attack"),
-      em("or"), strong("challenge"), em("to gun owners and those who believe and support the 2nd Amendment."),
-      em("Instead, the goal of our work to spurr healthy dialogue amongst all spectrums so that we can all work"),
-      em("together to find a solution. No kid should be afraid to attend school and no parent should have to fear"),
+      em("or"), strong("challenge"), em("to gun owners and
+                                        those who believe and
+                                        support the 2nd Amendment."),
+      em("Instead, the goal of our work to spurr healthy dialogue
+         amongst all spectrums so that we can all work"),
+      em("together to find a solution. No kid should be afraid
+         to attend school and no parent should have to fear"),
       em("for their child's safety."),
       h3("Meet The Team:")
-    ),
+      ),
     flowLayout(
-      div( class = "caption",
-        tags$img(src= '33895746_1417277445084602_4308802712805310464_n.jpg', height =200, width =200),
+      div(
+        class = "caption",
+        tags$img(
+          src = "33895746_1417277445084602_4308802712805310464_n.jpg",
+          height = 200, width = 200
+        ),
         p(em("Varun Patel"))
       ),
-      div( class = "caption",
-        tags$img(src= '33850842_961835110646127_8613901815181737984_n.jpg', height =200, width = 200),
+      div(
+        class = "caption",
+        tags$img(
+          src = "33850842_961835110646127_8613901815181737984_n.jpg",
+          height = 200, width = 200
+        ),
         p(em("Logan Selley"))
       ),
-      div( class = "caption",
-        tags$img(src= '33835367_2093832907496109_6306088030159503360_n.jpg', height = 200, width = 200),
+      div(
+        class = "caption",
+        tags$img(
+          src = "33835367_2093832907496109_6306088030159503360_n.jpg",
+          height = 200, width = 200
+        ),
         p(em("Trevor Leung"))
       ),
-      div( class = "caption",
-        tags$img(src = 'IMG_3992.jpg', height = 200, width = 200),
+      div(
+        class = "caption",
+        tags$img(
+          src = "IMG_3992.jpg",
+          height = 200, width = 200
+        ),
         p(em("Jose Aguirre"))
       )
     )
-  
-    
-    
-  ),
+      ),
   
   # Happiness tab
   tabPanel(
     "Happiness",
     sidebarLayout(
       sidebarPanel(
-        radioButtons("yaxis", "View Happiness Score by Rate of Gun Related Deaths per 100k People, Percent of Gun Ownership, or the Count of Gun Control Laws:",
-                     choiceNames = c("Rate of Gun Related Deaths per 100k People", "Percent of Gun Ownership", "Count of Gun Control Laws"),
+        radioButtons("yaxis", "View Happiness Score by Rate of
+                     Gun Related Deaths per 100k People,
+                     Percent of Gun Ownership, or the
+                     Count of Gun Control Laws:",
+                     choiceNames = c(
+                       "Rate of Gun Related Deaths per 100k People",
+                       "Percent of Gun Ownership", "Count of Gun Control Laws"
+                     ),
                      choiceValues = c("rate", "percown", "lawtotal")
-                     
-                     
         )
-      ),
-      mainPanel(
-        div(class = "center",
-          h1("Comparing State Happiness with Gun Related Data"),
-          em("Are happier states "), strong("safer"),
-          em("? We took data from wallet hub where they took in multiple factors "),
-          em("like depression rates and average income to calculate a happiness score "),
-          em("for each state. We compared the happiness scores from this data with the "),
-          a("percentages of gun ownership per state",
-            href = "http://demographicdata.org/facts-and-figures/gun-ownership-statistics/"),
-          em(", "),
-          a("rate of gun related deaths per 100k people",
-            href = "https://www.cdc.gov/nchs/pressroom/sosmap/firearm_mortality/firearm.htm"),
-          em(" and the "),
-          a("count of gun control laws",
-            href = "https://www.statefirearmlaws.org/table.html"),
-          em(" to make insightful conclusions and correlations between these variables. "),
-          em("In order to learn more about how happiness score was calculated, follow this "),
-          a("link", href = "https://wallethub.com/edu/happiest-states/6959/#methodology"),
-          em("."),
-          h3("Interactive Scatter Plot"),
-          br(),
-          div( class = "plot",
-            plotlyOutput("scatter_plot")
-          ),
-          textOutput("cor"),
-          textOutput("cor_message"),
-          br(),
-          p("The following map is color coded by happiness rank. Useful information such as the happiness rank out of all the states, gun industry rank, which ranks the states by how much a state relies on the firearm industry, and total victims of mass shootings."),
-          h3("Interactive Map")
+    ),
+    mainPanel(
+      div(
+        class = "center",
+        h1("Comparing State Happiness with Gun Related Data"),
+        em("Are happier states "), strong("safer"),
+        em("? We took data from wallet hub where
+           they took in multiple factors "),
+        em("like depression rates and average income
+           to calculate a happiness score "),
+        em("for each state. We compared the happiness scores
+           from this data with the "),
+        a("percentages of gun ownership per state",
+          href = "http://demographicdata.org/facts-and
+          -figures/gun-ownership-statistics/"
         ),
+        em(", "),
+        a("rate of gun related deaths per 100k people",
+          href = "https://www.cdc.gov/nchs/pressroom/sosmap
+          /firearm_mortality/firearm.htm"
+        ),
+        em(" and the "),
+        a("count of gun control laws",
+          href = "https://www.statefirearmlaws.org/table.html"
+        ),
+        em(" to make insightful conclusions and correlations
+           between these variables. "),
+        em("In order to learn more about how happiness score
+           was calculated, follow this "),
+        a("link", href = "https://wallethub.com/edu/happiest
+          -states/6959/#methodology"),
+        em("."),
+        h3("Interactive Scatter Plot"),
         br(),
-        div( class = "plot",
-          plotlyOutput("interactive_map")
+        plotlyOutput("scatter_plot"),
+        textOutput("cor"),
+        textOutput("cor_message"),
+        br(),
+        p("The following map is color coded by happiness rank.
+          Useful information such as
+          the happiness rank out of all the states, gun industry rank,
+          which ranks the states
+          by how much a state relies on the firearm industry,
+          and total victims of mass shootings."),
+        h3("Interactive Map")
+        ),
+      br(),
+      plotlyOutput("interactive_map")
         )
-      )
-    )
-  ),
-  #bar chart
+        )
+        ),
+  # bar chart
   tabPanel(
     "Legislation",
     sidebarLayout(
       sidebarPanel(
-        radioButtons("legislation", label = h3("Legislation Comparisons"),
-          choices = list("Gun Industry" = 1, "Gun Ownership" = 2, "Violent Crime" = 3, "Murder & Manslaughter" = 4) 
-          )
+        radioButtons("legislation",
+                     label = h3("Legislation Comparisons"),
+                     choices = list(
+                       "Gun Industry" = 1, "Gun Ownership" = 2,
+                       "Violent Crime" = 3,
+                       "Murder & Manslaughter" = 4
+                     )
+        )
       ),
       mainPanel(
-        div(class = "center",
+        div(
+          class = "center",
           h1("Analyzing the effects of State Legislated Gun Control"),
-          em("Due to a lack of federal action, many have looked to state legislatures to pass gun control laws"),
-          em(" in order to stem the epidemic of gun violence in the country. But is this legislation effective"),
-          em(" in keeping our communities safe? And what might be keeping state legislatures from implementing"),
+          em("Due to a lack of federal action, many have looked to state
+             legislatures to pass gun control laws"),
+          em(" in order to stem the epidemic of gun violence in the country.
+             But is this legislation effective"),
+          em(" in keeping our communities safe? And what might be keeping
+             state legislatures from implementing"),
           em(" this reform?"),
           br(),
           br(),
           strong("DATA & METHODOLOGY:"),
           br(),
-          a("Mother Jones mass shooting data", href = "https://www.motherjones.com/politics/2012/12/mass-shootings-mother-jones-full-data/"),
-          br(),
-          a("WalletHub gun industry data", href = "https://wallethub.com/edu/states-most-dependent-on-the-gun-industry/18719/"),
-          br(),
-          a("State Firearm Laws", href = "https://www.statefirearmlaws.org/table.html"),
-          br(),
-          a("State gun crime/ownership", href = "http://demographicdata.org/facts-and-figures/gun-ownership-statistics/"),
-          br(),
-          br(),
-          div( class = "plot",
-            plotlyOutput("legis_scatter")
+          a("Mother Jones mass shooting data",
+            href = "https://www.motherjones.com/politics/2012/12/mass
+            -shootings-mother-jones-full-data/"
           ),
+          br(),
+          a("WalletHub gun industry data",
+            href = "https://wallethub.com/edu/states-most-
+            dependent-on-the-gun-industry/18719/"
+          ),
+          br(),
+          a("State Firearm Laws",
+            href = "https://www.statefirearmlaws.org/table.html"
+          ),
+          br(),
+          a("State gun crime/ownership",
+            href = "http://demographicdata.org/facts-and
+            -figures/gun-ownership-statistics/"
+          ),
+          br(),
+          br(),
+          plotlyOutput("legis_scatter"),
           textOutput("legis_cor"),
           textOutput("legis_cor_message"),
           br(),
           br(),
-          div( class = "plot",
-            plotlyOutput("legislation_bar")
+          plotlyOutput("legislation_bar")
           )
-        )
-      )
-    )
+          )
+          )
   ),
   
-  # Conslusions
+  # References
   tabPanel(
-    "Conclusion",
-    sidebarLayout(
-      sidebarPanel(),
-      mainPanel()
-    )
-  )
-  
-)
+    "References",
+    tags$div(tags$ul(
+      tags$li(a("WalletHub",
+                href = "https://wallethub.com/edu/happiest-states/6959/#methodology"
+      )),
+      br(),
+      tags$li(a("DemographicData.org",
+                href = "https://wallethub.com/edu/happiest-states/6959/#methodology"
+      )),
+      br(),
+      tags$li(a("Centers for Disease Control and Prevention",
+                href = "https://www.cdc.gov/nchs/pressroom
+                /sosmap/firearm_mortality/firearm.htm"
+      )),
+      br(),
+      tags$li(a("State Firearm Laws",
+                href = "https://www.statefirearmlaws.org/table.html"
+      )),
+      br(),
+      tags$li(a("Mother Jones mass shooting data",
+                href = "https://www.motherjones.com/
+                politics/2012/12/mass-shootings-mother-jones-full-data/"
+      )),
+      br(),
+      tags$li(a("WalletHub gun industry data",
+                href = "https://wallethub.com/edu/
+                states-most-dependent-on-the-gun-industry/18719/"
+      )),
+      br(),
+      tags$li(a("State Firearm Laws",
+                href = "https://www.statefirearmlaws.org/table.html"
+      )),
+      br(),
+      tags$li(a("State gun crime/ownership",
+                href = "http://demographicdata.org/
+                facts-and-figures/gun-ownership-statistics/"
+      ))
+      ))
+      )
+      )
+
+
 
 
 
@@ -187,3 +278,16 @@ ui <- navbarPage(
 
 
 shinyUI(ui)
+
+
+
+
+
+
+
+
+
+
+
+
+
